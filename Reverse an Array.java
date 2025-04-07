1feaf0f7ec03864f07abbec0d770a97b30ed9cb2
pranjal030404 <pranjalp160@gmail.com>
2025-04-08 00:41:55 +0530

    Reverse an Array

---
+++ Reverse an Array.java
@@ -0,0 +1,23 @@
+You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Note: Modify the array in place.
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            // Swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+        
+    }
+}
