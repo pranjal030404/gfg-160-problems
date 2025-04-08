67d27d12b0aa4451a35e41704c252e7b74e62abe
pranjal030404 <pranjalp160@gmail.com>
2025-04-08 23:31:49 +0530

    Max Circular Subarray Sum

---
+++ Day 12 Max Circular Subarray Sum.java
@@ -0,0 +1,23 @@
+Given an array of integers arr[] in a circular fashion. Find the maximum subarray sum that we can get if we assume the array to be circular.
+
+
+class Solution {
+    // Function to find maximum circular subarray sum
+    public int circularSubarraySum(int[] arr) {
+        int n = arr.length;
+        int mn = Integer.MAX_VALUE, mx = Integer.MIN_VALUE;
+        int sum1 = 0, sum2 = 0, total = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += arr[i];
+            sum2 = Math.min(arr[i], sum2 + arr[i]);
+            mn = Math.min(mn, sum2);
+            sum1 = Math.max(arr[i], sum1 + arr[i]);
+            mx = Math.max(mx, sum1);
+        }
+
+        if (mx < 0) return mx;
+
+        return Math.max(mx, total - mn);
+    }
+}
