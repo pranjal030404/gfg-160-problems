840e575d677ca8fb106c12e83197dc5c5152311c
pranjal030404 <pranjalp160@gmail.com>
2025-04-08 23:25:40 +0530

    Minimize the Heights II done

--- Day 9 Minimize the Heights II.java
+++ Day 9 Minimize the Heights II.java
@@ -12,3 +12,21 @@
 // Note: It is compulsory to increase or decrease the height by K for each
 // tower. After the operation, the resultant array should not contain any
 // negative integers.
+
+class Solution {
+    int getMinDiff(int[] arr, int k) {
+        // code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        int r = arr[n - 1] - arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] >= k) {
+                int maxVal = Math.max(arr[i - 1] + k, arr[n - 1] - k);
+                int minVal = Math.min(arr[i] - k, arr[0] + k);
+                r = Math.min(r, maxVal - minVal);
+            }
+        }
+        return r;
+    }
+}
