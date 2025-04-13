46fa2b2016138f3482bac18b7651b113fb9ba7ce
pranjal030404 <pranjalp160@gmail.com>
2025-04-13 19:06:03 +0530

    Min Chars to Add for Palindrome

---
+++ Day 19 Min Chars to Add for Palindrome.java
@@ -0,0 +1,36 @@
+Given a string s, the task is to find the minimum characters to be added at the front to make the string palindrome.
+
+Note: A palindrome string is a sequence of characters that reads the same forward and backward.
+
+class Solution {
+    public static int minChar(String s) {
+        // Write your code here
+        String rev = new StringBuilder(s).reverse().toString();
+        String combined = s + "$" + rev;
+        int[] lps = computeLPSArray(combined);
+        return s.length() - lps[combined.length() - 1];
+    }
+
+    private static int[] computeLPSArray(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0;  
+        int i = 1;
+
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
