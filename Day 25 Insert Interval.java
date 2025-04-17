a9ffb50981cdc8412acd64d4c75f9f0e98ee0271
pranjal030404 <pranjalp160@gmail.com>
2025-04-17 21:44:07 +0530

    Insert Interval

---
+++ Day 25 Insert Interval.java
@@ -0,0 +1,27 @@
+// Geek has an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith event and intervals is sorted in ascending order by starti. He wants to add a new interval newInterval= [newStart, newEnd] where newStart and newEnd represent the start and end of this interval.
+
+// Help Geek to insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+class Solution {
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        // code here
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return result;
+    }
+}
