e638d53375506cd25a9d148abfe3675ad3ca45d2
pranjal030404 <pranjalp160@gmail.com>
2025-04-21 15:50:43 +0530

    Sorted and Rotated Minimum

---
+++ Day 29 Sorted and Rotated Minimum.java
@@ -0,0 +1,21 @@
+// A sorted array of distinct elements arr[] is rotated at some unknown point, the task is to find the minimum element in it. 
+
+
+
+
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+        int low = 0, high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return arr[low];
+    }
+}
