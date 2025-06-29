90a711b91d19a7058ea0e9ecabdb60f11115e904
pranjal030404 <pranjalp160@gmail.com>
2025-06-29 23:39:40 +0530

    Longest valid Parentheses

--- Day 100 Longest valid Parentheses.java
+++ Day 100 Longest valid Parentheses.java
@@ -1,4 +1,9 @@
+// Given a string s consisting of opening and closing parenthesis '(' and ')'. Find the length of the longest valid parenthesis substring.
 
+// A parenthesis string is valid if:
+
+// For every opening parenthesis, there is a closing parenthesis.
+// The closing parenthesis must be after its opening parenthesis.
 
 
 
