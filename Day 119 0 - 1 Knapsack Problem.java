6c481d35c501deeda436cf351deb4b9692188178
Pranjal Tiwari <pranjaltiwari69058@gmail.com>
2025-07-18 23:53:32 +0530

    0 - 1 Knapsack Problem

--- Day 119 0 - 1 Knapsack Problem.java
+++ Day 119 0 - 1 Knapsack Problem.java
@@ -1,3 +1,25 @@
 // Given n items, each with a specific weight and value, and a knapsack with a capacity of W, the task is to put the items in the knapsack such that the sum of weights of the items <= W and the sum of values associated with them is maximized. 
 
 // Note: You can either place an item entirely in the bag or leave it out entirely. Also, each item is available in single quantity.
+
+class Solution {
+    static int knapsack(int W, int val[], int wt[]) {
+        int n = val.length;
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                        val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                        dp[i - 1][w]
+                    );
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+}
